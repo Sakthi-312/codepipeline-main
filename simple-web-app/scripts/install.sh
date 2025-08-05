72703285f6890159e9de736e4225efa2d15ca00b
Sakthi-312 <sakthivel.m3125@gmail.com>
2025-08-05 20:21:08 +0530

    Update install.sh

--- simple-web-app/scripts/install.sh
+++ simple-web-app/scripts/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-set -e
-exec > /tmp/install.log 2>&1
-
-apt-get update
-apt-get install -y apache2
-systemctl start apache2
-systemctl enable apache2
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
